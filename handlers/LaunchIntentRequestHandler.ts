8c5804ae734f331527eb2f205c8ceb7052816ab9
Dan Johnson <dan@designmynight.com>
2018-05-10 16:21:39 +0100

    feat: added correct response messages to starting the game

--- handlers/LaunchIntentRequestHandler.ts
+++ handlers/LaunchIntentRequestHandler.ts
@@ -1,12 +1,25 @@
 import { Response } from 'ask-sdk-model'
 import { RequestHandler, HandlerInput } from 'ask-sdk-core'
+import GameService from '../services/GameService';
 
 export default class LaunchIntentRequestHandler implements RequestHandler {
     canHandle (handlerInput: HandlerInput): boolean {
+        const request = handlerInput.requestEnvelope.request
 
+        return (request.type === 'IntentRequest' && request.intent.name === 'NewGameIntent') 
+            || request.type === 'LaunchRequest'
     }
 
     handle (handlerInput: HandlerInput): Response {
+        const gameService = new GameService(handlerInput.attributesManager)
+        const welcomeMessage = 'Welcome. I\'m going to give you some quotes - some will have been said by Trump, some will not. Give me your answer by responding with "True" or "False".'
+        const firstQuestion = 'Here\'s your first question:'
+        const quote = gameService.getQuote()
+        const constructedMessage = welcomeMessage + ' ' + firstQuestion + ' ' + quote
 
+        return handlerInput.responseBuilder
+            .speak(constructedMessage)
+            .reprompt(quote)
+            .getResponse()
     }
 }
