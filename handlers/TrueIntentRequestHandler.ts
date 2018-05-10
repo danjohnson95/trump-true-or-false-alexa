144bf9d68748de150e5e9c4fa4be9082619ab4ba
Dan Johnson <dan@designmynight.com>
2018-05-10 15:52:05 +0100

    feat: created request handlers for intents

---
+++ handlers/TrueIntentRequestHandler.ts
@@ -0,0 +1,12 @@
+import { Response } from 'ask-sdk-model'
+import { RequestHandler, HandlerInput } from 'ask-sdk-core'
+
+export default class TrueIntentRequestHandler implements RequestHandler {
+    canHandle (handlerInput: HandlerInput): boolean {
+
+    }
+
+    handle (handlerInput: HandlerInput): Response {
+
+    }
+}
