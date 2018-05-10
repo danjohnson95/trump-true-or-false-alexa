7c231962f3fe5acee7501ee7ec9090f9408a8db6
Dan Johnson <dan@designmynight.com>
2018-05-10 16:21:07 +0100

    feat: session now stores questions as they get asked

--- services/GameService.ts
+++ services/GameService.ts
@@ -1,18 +1,29 @@
 import * as TrueQuotes from '../data/TrueQuotes'
 import * as FalseQuotes from '../data/FalseQuotes'
+import { AttributesManager } from 'ask-sdk-core';
 
 export default class GameService {
-    private sessionData: any
+    private attributesManager: AttributesManager
 
-    constructor (sessionData: any) {
-        this.sessionData = sessionData
+    constructor (attributesManager: AttributesManager) {
+        this.attributesManager = attributesManager
     }
 
-    getQuote (): string {
+    public getQuote (): string {
         // Should we grab a true or a false?
-        const answer = !!Math.round(Math.random())
+        const decision = !!Math.round(Math.random())
+        const quote = decision ? this.getRandomTrueQuote() : this.getRandomFalseQuote()
 
-        return answer ? this.getRandomTrueQuote() : this.getRandomFalseQuote()
+        this.storeQuestion(quote, decision)
+
+        return quote
+    }
+
+    private storeQuestion (quote: string, answer: boolean): void {
+        this.attributesManager.setSessionAttributes({
+            lastQuestion: quote,
+            answer: answer
+        })
     }
 
     private getRandomTrueQuote (): string {
